7f3bed12955e5d143b1a45761082d4bbc5d78b3f
Daniel Allen <dan@aeternagroup.net>
2026-07-06 23:12:36 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -2,4 +2,16 @@ output "email_communication_service_domain_sender_usernames" {
   description = "All email_communication_service_domain_sender_username resources"
   value       = azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames
 }
+output "email_communication_service_domain_sender_usernames_display_name" {
+  description = "List of display_name values across all email_communication_service_domain_sender_usernames"
+  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.display_name]
+}
+output "email_communication_service_domain_sender_usernames_email_service_domain_id" {
+  description = "List of email_service_domain_id values across all email_communication_service_domain_sender_usernames"
+  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.email_service_domain_id]
+}
+output "email_communication_service_domain_sender_usernames_name" {
+  description = "List of name values across all email_communication_service_domain_sender_usernames"
+  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.name]
+}
 
