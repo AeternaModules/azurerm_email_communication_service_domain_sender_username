f973ebc3f48a9cb2b1debfbfc93b4f76d8d2a92a
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:17:27 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "email_communication_service_domain_sender_usernames" {
-  description = "All email_communication_service_domain_sender_username resources"
-  value       = azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames
-}
 output "email_communication_service_domain_sender_usernames_display_name" {
-  description = "List of display_name values across all email_communication_service_domain_sender_usernames"
-  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.display_name]
+  description = "Map of display_name values across all email_communication_service_domain_sender_usernames, keyed the same as var.email_communication_service_domain_sender_usernames"
+  value       = { for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : k => v.display_name }
 }
 output "email_communication_service_domain_sender_usernames_email_service_domain_id" {
-  description = "List of email_service_domain_id values across all email_communication_service_domain_sender_usernames"
-  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.email_service_domain_id]
+  description = "Map of email_service_domain_id values across all email_communication_service_domain_sender_usernames, keyed the same as var.email_communication_service_domain_sender_usernames"
+  value       = { for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : k => v.email_service_domain_id }
 }
 output "email_communication_service_domain_sender_usernames_name" {
-  description = "List of name values across all email_communication_service_domain_sender_usernames"
-  value       = [for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : v.name]
+  description = "Map of name values across all email_communication_service_domain_sender_usernames, keyed the same as var.email_communication_service_domain_sender_usernames"
+  value       = { for k, v in azurerm_email_communication_service_domain_sender_username.email_communication_service_domain_sender_usernames : k => v.name }
 }
 
